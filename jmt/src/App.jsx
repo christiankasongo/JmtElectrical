bca83897f7ae637dc51edc7f820ee9e610795d32
Christian Kasongo <christiankasongo03@gmail.com>
2023-01-14 18:19:37 +0000

    MUI installed
    Using MUI for app bar and other components.

--- jmt/src/App.jsx
+++ jmt/src/App.jsx
@@ -1,36 +1,14 @@
 import React from "react";
-
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-
-import logo from "./assets/logo.png";
-
-import "bootstrap/dist/css/bootstrap.min.css";
+import TopBar from "./Components/Topbar";
 
 const App = () => {
   return (
     <>
       <div>
         <header>
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
-            <Container>
-              <Navbar.Brand className="me-2" href="#home">
-                <img src={logo} alt="Logo" height="70px" />
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link href="#features">Home</Nav.Link>
-                  <Nav.Link href="#pricing">About</Nav.Link>
-                  <Nav.Link href="#pricing">Services</Nav.Link>
-                  <Nav.Link href="#pricing">Contact</Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
+          <TopBar/>
         </header>
+        <div>Helllooo</div>
       </div>
     </>
   );
