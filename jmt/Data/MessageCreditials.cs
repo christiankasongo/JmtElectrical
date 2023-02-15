dcfc393f478cbd2f06fb3d444d7001f0b355ceb8
ckasongo <ckasongo@sidetrade.com>
2023-02-15 12:21:33 +0000

    Modified navigation system

---
+++ jmt/Data/MessageCreditials.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace jmt.Data
+{
+    public class MessageCreditials
+    {
+        [Required(ErrorMessage = "This field is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        public string Message { get; set; }
+    }
+}
